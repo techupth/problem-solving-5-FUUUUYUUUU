84c565a66b38c4457422844121013327b7679af2
Panupong <Pai28212m@gmail.com>
2024-02-05 15:56:56 +0700

    finish

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function runningSum(nums) {
-    
-};
+  for (let i = 0; i < nums.length; i++) {
+    if (i == 0) {
+      nums[i] = nums[i];
+    } else {
+      nums[i] += nums[i - 1];
+    }
+  }
+  return nums;
+}
+
+
+let nums = [3,1,2,10,1];
+
+nums = runningSum(nums);
+
+console.log(nums);
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,24 @@
 function moveZeroes(nums) {
-    
-};
+  let zeroCount = 0;
+  const lastIndex = nums.length - 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      zeroCount++;
+    }
+
+    if (nums[i] != 0) {
+      const swapValue = nums[i - zeroCount];
+      nums[i - zeroCount] = nums[i];
+      nums[i] = swapValue;
+    }
+  }
+
+  return nums;
+}
+
+let nums = [0, 1, 0, 3, 12];
+
+nums = moveZeroes(nums);
+
+console.log(nums);
